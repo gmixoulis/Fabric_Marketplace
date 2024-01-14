485f40ab411ed0342958fc81620b09450e4dbb9b
George Niotis <geoniotis@yahoo.com>
2024-01-14 17:17:02 +0200

    fix: move files

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ SPDX-License-Identifier: Apache-2.0
 
 'use strict';
 
-const tokenERC721Contract = require('./lib/tokenERC721.js');
+// const tokenERC721Contract = require('./lib/tokenERC721.js');
 const FractionToken = require('./lib/FractionToken.js');
 
-module.exports.tokenERC721Contract = tokenERC721Contract;
+// module.exports.tokenERC721Contract = tokenERC721Contract;
 module.exports.FractionToken = FractionToken;
 module.exports.contracts = [FractionToken];
