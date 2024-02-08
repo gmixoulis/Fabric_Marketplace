92f2e5be755b4d0a66307bc64613dfd4a42bdd4f
George Michoulis <gmichoulis@sidroco.com>
2024-02-08 14:25:36 +0200

    Update index.js

--- contracts/index.js
+++ contracts/index.js
@@ -5,8 +5,11 @@ SPDX-License-Identifier: Apache-2.0
 'use strict';
 
 const tokenERC721Contract = require('./lib/tokenERC721.js');
-const tokenERC20Contract = require('./lib/tokenERC20.js');
+const marketplaceContract = require('./lib/marketplace.js');
 
 module.exports.tokenERC721Contract = tokenERC721Contract;
-module.exports.TokenERC20Contract = tokenERC20Contract;
-module.exports.contracts = [tokenERC721Contract,tokenERC20Contract];
+module.exports.marketplaceContract = marketplaceContract;
+module.exports.contracts = [
+    marketplaceContract,
+    tokenERC721Contract
+  ];
